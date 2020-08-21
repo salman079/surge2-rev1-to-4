d1e14dcb4bf6a2669de758afdf1b0949c69ab4ae
salmansiddiqui <salman079@gmail.com>
2020-08-21 15:04:36 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+
 
 function App() {
+  let [cal, setCal]= useState(0)
+  let [mood, setMood] = useState(true)
   return (
     <div className="App">
+      <h1> <center>Revision of Class 1 to 4 </center></h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> testing </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> testing </p>     
       </header>
+      <body className={`bgcol-good ${mood ? '' : 'bgcol-bad'}`}>
+        <h1>Counter Result {cal}</h1>
+        <button onClick={()=>alert('testing alert')}> Alert !!!   </button> <br />
+        <button onClick={()=>setCal(cal+1)}> Addition   </button> 
+        <button onClick={()=>setCal(cal-1)}> Subtraction   </button> <br />
+        <h1>Current Mood {mood ? 'Good' : 'Bad'}</h1><br />
+        <button onClick={()=>setMood(!mood)}> Switching Mood   </button> <br />
+      </body>
     </div>
   );
 }
